ef472c8487164e97982628fa62772b1ed1329f3c
AndreDutraDCC <54049278+AndreDutraDCC@users.noreply.github.com>
2025-06-05 21:00:35 +0000

    Deploying to gh-pages from @ AndreDutraDCC/andredutradcc.github.io@0cc14b504d9565d03bff8854e482886e311ca37d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-sobre",
+    title: "sobre",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-currículo",
+          title: "Currículo",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
