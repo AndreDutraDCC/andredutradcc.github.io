14abf3b12a7e94f5d90f234dc215b2fc27caf5a9
AndreDutraDCC <54049278+AndreDutraDCC@users.noreply.github.com>
2025-06-06 18:57:27 +0000

    Deploying to gh-pages from @ AndreDutraDCC/andredutradcc.github.io@98ef444848a0a8b7257d6b96b1f9a31906fe6b31 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-currículo",
+  },{id: "nav-projetos",
+          title: "projetos",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-currículo",
           title: "currículo",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
